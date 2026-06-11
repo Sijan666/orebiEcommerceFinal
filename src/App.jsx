17099f9bf68698afd5749a25ea8e29b8af2580c4
Sijan666 <666majharulislam@gmail.com>
2026-06-11 22:05:31 +0600

    add lenis for premium scrolling behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import ReactLenis from 'lenis/react'
+
+// Components
 import Home from './components/pages/Home'
-import { Routes , Route } from 'react-router-dom'
 import RootLayouts from './components/layouts/RootLayouts'
 import About from './components/pages/About'
 import Error from './components/pages/Error'
@@ -11,27 +14,24 @@ import Login from './components/pages/Login'
 import Signup from './components/pages/Signup'
 import ProductInside from './components/pages/ProductInside'
 
-
-function App () {
+function App() {
   return (
-    <>
+    <ReactLenis root>
       <Routes>
         <Route path="/" element={<RootLayouts />}>
-          <Route index element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/shop" element={<Shop/>} />
-          <Route path="/contact" element={<ContactUs/>} />
-          <Route path="/journal" element={<Journal/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/productinside" element={<ProductInside/>} />
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/journal" element={<Journal />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/productinside" element={<ProductInside />} />
         </Route>
-          <Route path="*" element={<Error/>} />
+        <Route path="*" element={<Error />} />
       </Routes>
-    </>
+    </ReactLenis>
   )
 }
 
 export default App
-
-
