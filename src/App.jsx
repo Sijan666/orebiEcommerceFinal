b34b5515ab6507fe76c8d5ed02a0177c1e70adb3
Sijan666 <666majharulislam@gmail.com>
2026-06-11 22:09:29 +0600

    add lazy loading for better perfomance

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
+import React, { Suspense, lazy } from 'react'
 import './App.css'
-import { ReactLenis } from '@studio-freight/react-lenis'
+import { ReactLenis } from 'lenis/react'
 import { Routes, Route } from 'react-router-dom'
 
-// Components
-import Home from './components/pages/Home'
 import RootLayouts from './components/layouts/RootLayouts'
-import About from './components/pages/About'
-import Error from './components/pages/Error'
-import Shop from './components/pages/Shop'
-import ContactUs from './components/pages/ContactUs'
-import Journal from './components/pages/Journal'
-import Login from './components/pages/Login'
-import Signup from './components/pages/Signup'
-import ProductInside from './components/pages/ProductInside'
+
+const Home = lazy(() => import('./components/pages/Home'))
+const About = lazy(() => import('./components/pages/About'))
+const Shop = lazy(() => import('./components/pages/Shop'))
+const ContactUs = lazy(() => import('./components/pages/ContactUs'))
+const Journal = lazy(() => import('./components/pages/Journal'))
+const Login = lazy(() => import('./components/pages/Login'))
+const Signup = lazy(() => import('./components/pages/Signup'))
+const ProductInside = lazy(() => import('./components/pages/ProductInside'))
+const Error = lazy(() => import('./components/pages/Error'))
 
 function App() {
   return (
-    <ReactLenis 
-      root 
-      options={{
-        lerp: 0.05,
-        wheelMultiplier: 0.8,
-        smoothWheel: true,
-        syncTouch: true,
-      }}
-    >
-      <Routes>
-        <Route path="/" element={<RootLayouts />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/journal" element={<Journal />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/productinside" element={<ProductInside />} />
-        </Route>
-        <Route path="*" element={<Error />} />
-      </Routes>
+    <ReactLenis root options={{lerp: 0.05, wheelMultiplier: 0.8, smoothWheel: true, syncTouch: true,}}>
+      <Suspense fallback={<div className="loading-screen">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<RootLayouts />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/journal" element={<Journal />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/productinside" element={<ProductInside />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
     </ReactLenis>
   )
 }
