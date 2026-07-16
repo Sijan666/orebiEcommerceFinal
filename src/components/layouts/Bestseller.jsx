3cf5728ce1e3e28f814c9c075054981a7203ee17
Sijan666 <666majharulislam@gmail.com>
2026-07-16 19:09:08 +0600

    fix responsive issues in Bestseller

--- src/components/layouts/Bestseller.jsx
+++ src/components/layouts/Bestseller.jsx
@@ -6,7 +6,6 @@ import axios from "axios";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 const Bestseller = () => {
     let [allData, setAllData] = useState([]);
 
@@ -22,24 +21,24 @@ const Bestseller = () => {
         allDatas();
     }, []);
 
-
-
     return (
-        <div className="pt-8 pb-20">
+        <div className="pt-8 pb-10 md:pb-16 lg:pb-20">
             <Container>
-                <h3 className="text-[39px] text-[#262626] font-bold block pb-[30px]">Our Bestsellers</h3>
-                    <div className='grid lg:grid-cols-4 grid-cols-2 gap-3'>
-                        {allData.slice(0,4).map((item)=>(
-                        <div key={item.id}>
-                            <Product
-                            productImg={item.thumbnail}
-                            badgeText={item.stock}
-                            productTitle={item.title}
-                            productPrice={item.price}
-                            />
-                        </div>
-                        ))} 
+                <h3 className="text-[26px] md:text-[32px] lg:text-[39px] text-[#262626] font-bold block pb-5 lg:pb-[30px] px-3 lg:px-0">
+                    Our Bestsellers
+                </h3>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-5 lg:gap-8 px-3 lg:px-0'>
+                    {allData.slice(0,4).map((item)=>(
+                    <div key={item.id} className="w-full">
+                        <Product
+                        productImg={item.thumbnail}
+                        badgeText={item.stock}
+                        productTitle={item.title}
+                        productPrice={item.price}
+                        />
                     </div>
+                    ))} 
+                </div>
             </Container>
         </div>
     );
