f4dd212fc9a692274cfd84785642fe2f65b72c56
Sijan666 <666majharulislam@gmail.com>
2026-07-16 23:16:04 +0600

    delete unused component

--- src/components/pages/Journal.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Journal = () => {
-    return (
-        <div className="bg-amber-500 text-center text-6xl font-bold py-10 text-white">Welcome To Journal Page</div>
-    )
-}
-
-export default Journal
