256d2ca966e4d3f56f7108b1b2c1321630e6ce76
Sijan666 <666majharulislam@gmail.com>
2026-07-20 23:44:42 +0600

    add helmet async in main

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import store from './app/store'
 import { Provider } from 'react-redux'
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>,
 )
