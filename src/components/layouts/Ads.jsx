c4d67961358fdf6d30f82841440fb5c2fcd16416
Sijan666 <666majharulislam@gmail.com>
2026-07-24 02:09:29 +0600

    fix : add aria-labels to advertisement image links

--- src/components/layouts/Ads.jsx
+++ src/components/layouts/Ads.jsx
@@ -13,20 +13,20 @@ const Ads = () => {
                 <Flex className="flex-col lg:flex-row flex-wrap lg:flex-nowrap lg:justify-between lg:gap-x-8 gap-y-4 sm:gap-y-6 md:gap-y-8 lg:gap-y-0">
                     {/* Left Banner */}
                     <div className="lg:leftSide w-full lg:w-1/2 relative flex justify-center">
-                        <Link to={'/shop'} className="w-full">
-                        <Images imgSrc={sh1} />
+                        <Link to={'/shop'} className="w-full" aria-label="Shop our pendant lamps sale">
+                            <Images imgSrc={sh1} alt="Pendant lamps sale" />
                         </Link>
                     </div>
                     {/* Right Banners */}
                     <div className="lg:rightSide w-full lg:w-1/2 relative">
                         <div className="lg:pb-8 w-full flex justify-center">
-                            <Link to={'/shop'} className="w-full">
-                            <Images imgSrc={sh2} />
+                            <Link to={'/shop'} className="w-full" aria-label="Shop electronics and clocks offer">
+                                <Images imgSrc={sh2} alt="Electronics sale" />
                             </Link>
                         </div>
                         <div className='mt-4 sm:mt-6 md:mt-8 lg:mt-0 w-full flex justify-center'>
-                            <Link to={'/shop'} className="w-full">
-                            <Images imgSrc={sh3} />
+                            <Link to={'/shop'} className="w-full" aria-label="Shop furniture and lighting offer">
+                                <Images imgSrc={sh3} alt="Furniture offer" />
                             </Link>
                         </div>
                     </div>
